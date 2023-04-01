8427540814780328572694b16a8b71ef249ef0c1
Ulyana Lakevich <ulyanalakevich@gmail.com>
2023-04-01 20:30:54 +0200

    store and Provider added

--- src/index.js
+++ src/index.js
@@ -3,54 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import GlobalStyle from './GlobalStyle';
-import { configureStore } from '@reduxjs/toolkit';
-const initialState = {
-  tasks: [],
-};
-
-const tasksReducer = (state = initialState, action) => {
-
-// switch (action.type) {
- // case "addTask":
-// }
-if(action.type === "addTodo") {
-  return {
-      ...state,
-      tasks: [
-          ...state.tasks,
-          {
-              content: action.payload,
-          },
-      ],
-  };
-  // default: 
-  // return state;
-}
-
-return state;
-};
-
-const addTask = content => ({
-  type: "addTodo",
-  payload:content,
-});
-
-const selectTasks = ({tasks}) => tasks;
-
-const store = configureStore({reducer: tasksReducer});
-console.log(selectTasks(store.getState()));
-
-store.dispatch(addTask("zadanie 1"));
-console.log(selectTasks(store.getState()));
-
-store.dispatch(addTask("zadanie 2"));
-console.log(store.getState());
+import store from "./store"
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <GlobalStyle/>
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
