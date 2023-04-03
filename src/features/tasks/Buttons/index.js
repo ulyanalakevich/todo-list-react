c28735494d1ef41e5693b5f80d0020cf42246d75
Ulyana Lakevich <ulyanalakevich@gmail.com>
2023-04-03 17:48:57 +0200

    reducers from tasksSlice added

--- src/features/tasks/Buttons/index.js
+++ src/features/tasks/Buttons/index.js
@@ -1,21 +1,27 @@
+import { selectTasks, toggleHideDone, setAllDone } from "../tasksSlice";
 import { Content, Button } from "./styled";
+import { useSelector, useDispatch } from "react-redux";
 
-const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => (
-  <Content>
-    {tasks.length > 0 && (
-      <>
-        <Button onClick={toggleHideDone} >
-          {hideDone ? "Pokaż" : "Ukryj"} ukończone
-        </Button>
-        <Button
-          disabled={tasks.every(({ done }) => done)}
-          onClick={setAllDone}
-        >
-          Ukończ wszystkie
-        </Button>
-      </>
-    )}
-  </Content>
-);
+const Buttons = () => {
+  const { tasks, hideDone } = useSelector(selectTasks);
+  const dispatch = useDispatch();
+  return (
+    <Content>
+      {tasks.length > 0 && (
+        <>
+          <Button onClick={() => dispatch(toggleHideDone())} >
+            {hideDone ? "Pokaż" : "Ukryj"} ukończone
+          </Button>
+          <Button
+            disabled={tasks.every(({ done }) => done)}
+            onClick={() => dispatch(setAllDone())}
+          >
+            Ukończ wszystkie
+          </Button>
+        </>
+      )}
+    </Content>
+  );
+}
 
 export default Buttons;
