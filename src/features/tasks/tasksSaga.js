8b52aca0cd571fc3e8c8bfa89c724c35d692a895
Ulyana Lakevich <ulyanalakevich@gmail.com>
2023-04-05 17:35:35 +0200

    Saga added

---
+++ src/features/tasks/tasksSaga.js
@@ -0,0 +1,25 @@
+import { takeLatest, takeEvery, call, put, delay, select } from "redux-saga/effects";
+import { fetchExampleTasks, selectTasks, setTasks } from "./tasksSlice";
+import { getExampleTasks } from "./getExampleTasks";
+import { saveTasksInLocalStorage } from "./tasksLocalStorage";
+
+function* fetchExampleTasksHandler() {
+    try {
+        yield delay(500);
+        const exampleTasks = yield call(getExampleTasks);
+        yield put(setTasks(exampleTasks));
+    } catch (error) {
+        yield call(alert, "Coś poszło nie tak!");
+    }
+}
+
+function* saveTasksInLocalStorageHandler() {
+    const tasks = yield select(selectTasks);
+    yield call(saveTasksInLocalStorage, tasks);
+}
+
+export function* tasksSaga() {
+    //yield throttle(2000,fetchExampleTasks.type, fetchExampleTasksHandler )
+    yield takeLatest(fetchExampleTasks.type, fetchExampleTasksHandler)
+    yield takeEvery("*", saveTasksInLocalStorageHandler);
+}
