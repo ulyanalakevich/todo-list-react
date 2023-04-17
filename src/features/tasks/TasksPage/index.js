ccf5aad9794b0d76e313ad508c24086418d385e2
Ulyana Lakevich <ulyanalakevich@gmail.com>
2023-04-17 17:51:28 +0200

    new folder TasksPage

---
+++ src/features/tasks/TasksPage/index.js
@@ -0,0 +1,38 @@
+import Form from "./Form";
+import TaskList from "./TaskList";
+import { Buttons, ExampleTasksButton } from "./Buttons";
+import Section from "../../../common/Section";
+import Header from "../../../common/Header";
+import Container from "../../../common/Container";
+import Search from "./Search";
+
+function TasksPage() {
+
+
+  return (
+    <Container>
+      <Header title="Lista zadań" />
+      <Section
+        title="Dodaj nowe zadanie"
+        body={<Form />}
+        extraHeaderContent={
+          <ExampleTasksButton />
+        }
+      />
+      <Section
+        title="Wyszukiwarka"
+        body={<Search />}
+      />
+
+      <Section
+        title="Lista zadań"
+        body={<TaskList />}
+        extraHeaderContent={
+          <Buttons />
+        }
+      />
+    </Container>
+  );
+}
+
+export default TasksPage;
