289b09f83fcf1ad89f5dfb9ac4ff6d618997c109
Ulyana Lakevich <ulyanalakevich@gmail.com>
2023-04-17 17:51:40 +0200

    new folder TaskPage

---
+++ src/features/tasks/TaskPage/index.js
@@ -0,0 +1,25 @@
+import Section from "../../../common/Section";
+import Header from "../../../common/Header";
+import Container from "../../../common/Container";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useSelector } from "react-redux";
+import { getTaskById } from "../tasksSlice";
+
+function TaskPage() {
+const {id} = useParams();
+const task = useSelector(state => getTaskById(state, id));
+
+  return (
+    <Container>
+      <Header title="Szczegóły zadania" />
+      <Section
+        title={task ? task.content : "Nie znaleziono zadania"}
+        body={
+        <><strong>Ukończono:</strong> {task.done ? "Tak" : "Nie"}</>
+    }
+      />
+    </Container>
+  );
+}
+
+export default TaskPage;
