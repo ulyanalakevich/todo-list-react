6f81d6c82960553914b4ecf81711820a7dd6d415
Ulyana Lakevich <ulyanalakevich@gmail.com>
2023-04-17 18:41:59 +0200

    HashRouter, Switch, Route added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,27 @@
-import Tasks from "./features/tasks/Tasks";
+import TasksPage from "./features/tasks/TasksPage";
+import TaskPage from "./features/tasks/TaskPage";
+import AuthorPage from "./features/author/AuthorPage";
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
+import { Navigation } from "./Navigation";
+
 
 export default () =>
-<Tasks/>;
+(
+  <HashRouter>
+    <Navigation />
+    <Switch>
+      <Route path="/zadania/:id">
+        <TaskPage />
+      </Route>
+      <Route path="/zadania">
+        <TasksPage />
+      </Route>
+      <Route path="/author">
+        <AuthorPage />
+      </Route>
+      <Route path="/">
+        <Redirect to="/zadania" />
+      </Route>
+    </Switch>
+  </HashRouter>
+);
