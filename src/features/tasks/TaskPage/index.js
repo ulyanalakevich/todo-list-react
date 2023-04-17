47a77098370f08cd14fe0a80c90fa4fc21936041
Ulyana Lakevich <ulyanalakevich@gmail.com>
2023-04-17 18:44:25 +0200

    maintenance of non-existent tasks added

--- src/features/tasks/TaskPage/index.js
+++ src/features/tasks/TaskPage/index.js
@@ -6,17 +6,17 @@ import { useSelector } from "react-redux";
 import { getTaskById } from "../tasksSlice";
 
 function TaskPage() {
-const {id} = useParams();
-const task = useSelector(state => getTaskById(state, id));
+  const { id } = useParams();
+  const task = useSelector(state => getTaskById(state, id));
 
   return (
     <Container>
       <Header title="Szczegóły zadania" />
       <Section
         title={task ? task.content : "Nie znaleziono zadania"}
-        body={
-        <><strong>Ukończono:</strong> {task.done ? "Tak" : "Nie"}</>
-    }
+        body={!!task && (
+          <><strong>Ukończono:</strong> {task.done ? "Tak" : "Nie"}</>
+        )}
       />
     </Container>
   );
