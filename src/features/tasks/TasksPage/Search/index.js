3bb57436c7599b8193f05038461d49b3df4c100e
Ulyana Lakevich <ulyanalakevich@gmail.com>
2023-04-17 18:43:18 +0200

    hooks to replace searchParams created

--- src/features/tasks/TasksPage/Search/index.js
+++ src/features/tasks/TasksPage/Search/index.js
@@ -1,23 +1,21 @@
 import { useHistory, useLocation } from "react-router-dom/cjs/react-router-dom.min";
 import Input from "../../Input"
 import { Wrapper } from "./styled";
-import searchQueryParamName from "../searchQueryParamName";
+import searchQueryParamName from "../queryParameters/searchQueryParamName";
+import { useQueryParameter, useReplaceQueryParameter } from "../queryParameters";
 
 export default () => {
 
-    const location = useLocation();
-    const history = useHistory();
-    const query = (new URLSearchParams(location.search)).get(searchQueryParamName);
+    // const query = (new URLSearchParams(location.search)).get(searchQueryParamName);
+    const query = useQueryParameter(searchQueryParamName);
+    const replaceQueryParameter = useReplaceQueryParameter();
+
     const onInputChange = ({ target }) => {
-        const searchParams = new URLSearchParams(location.search);
-
-        if (target.value.trim() === "") {
-            searchParams.delete(searchQueryParamName);
-        } else {
-            searchParams.set(searchQueryParamName, target.value);
-        }
-
-        history.push(`${location.pathname}?${searchParams.toString()}`);
+        // const searchParams = new URLSearchParams(location.search);
+        replaceQueryParameter({
+            key: searchQueryParamName,
+            value: target.value.trim() !== "" ? target.value : undefined,
+        });
     };
 
     return (
